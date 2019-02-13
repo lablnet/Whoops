09d4ec20fd3d533a300a21d28b6064e85e4ec8b9
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-02-13 14:29:44 +0500

    Update index.php

--- example/index.php
+++ example/index.php
@@ -2,6 +2,12 @@
 	use Lablnet\Whoops;
 	require '../vendor/autoload.php';
 	$whoops = new Whoops();
+
+	//append the editor and use that
+	//$whoops->appendEditor("whateverName" , "whateverUri://open?url=file://::file&line=::line",)->setEditor('test');
+
+	//use editor
+	$whoops->setEditor('sublime');
 	$a = 10;
 	echo "The value of a is ".valueOfA();
 	
